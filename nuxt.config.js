3a439ecac85dcec2acfc0c7eb13415eaddfc6a06
Erik Södermark <erik@sodermark.tech>
2022-05-29 16:12:12 +0200

    Add router-base for github pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,10 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
+  router: {
+    base: '/mo2-scribing/'
+  }
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'mo2-scribing',
