cc1d97a252cce66929a214cf311f1f1641e0b20a
Erik Södermark <erik@sodermark.tech>
2022-06-07 12:39:38 +0200

    Add title an favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'mo2-scribing',
+    title: 'MO2 Scribing',
     htmlAttrs: {
       lang: 'en',
     },
@@ -21,7 +21,7 @@ export default {
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/mo2-scribing/favicon.jpg' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
